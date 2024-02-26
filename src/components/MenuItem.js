f8f76185656c41c5e07af0fa4a4c5393d421dd32
tesfaa abi <abitesfahunn@gmail.com>
2024-02-26 23:13:40 +0300

    Update MenuItem.js

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,5 +1,9 @@
 export default function MenuItem({image}) {
-  return (
+
+
+
+  
+return (
     <div className="bg-gray-200 p-4 rounded-lg text-center group
      hover:bg-white hover:shadow-md hover:shadow-black/25  transition-all ">
         <div className="w-full h-64 relative overflow-hidden">
@@ -13,8 +17,6 @@ export default function MenuItem({image}) {
         <button className="bg-primary text-white rounded-full px-10 py-3 m-3 ">ይዘዙ</button>
     </div>
   )
-
-  
 }
 
 
